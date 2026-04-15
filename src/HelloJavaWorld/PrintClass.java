113930988223d8019e9d6ecb00c40099587d87cb
Dumitru <dumitru.iucalciuc@gmail.com>
2026-04-15 16:22:07 +0200

    Print Hello Java World

---
+++ src/HelloJavaWorld/PrintClass.java
@@ -0,0 +1,10 @@
+package HelloJavaWorld;
+
+public class PrintClass {
+    public PrintClass(){
+
+    }
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import HelloJavaWorld.PrintClass;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void main (String[] args) {
+        PrintClass FirstPrint = new PrintClass();
+        FirstPrint.printMessage("Hello Java World!");
+    }
 
 }
