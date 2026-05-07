3d59c1df30e2b43ea537ac56b1a72fd5ebfb5597
Dumitru <dumitru.iucalciuc@gmail.com>
2026-05-07 12:26:36 +0300

    HW 6 - Arrays (exercise 4 was not solved)

--- src/HW6/Hw6Exercise4.java
+++ src/HW6/Hw6Exercise4.java
@@ -15,43 +15,33 @@ Sum of the main diagonal: 15
  */
 public class Hw6Exercise4 {
     public static void exercise4 () {
-        int M = 0;
-        int N = 0;
-        int initialArray [][];
-        int transposeArray [][];
+        int size = 0;
+        int array [][];
 
         Scanner inputFromUser = new Scanner(System.in);
         do {
-            System.out.println("Enter the size of the MxN matrix: ");
-            M = inputFromUser.nextInt();
+            System.out.println("Enter the size of the square matrix: ");
+            size = inputFromUser.nextInt();
             inputFromUser.nextLine();
-            if (M <= 0) System.out.println("Wrong input: M > 0");
-        }while (M <= 0);
+            if (size < 1) System.out.println("Wrong input: size > 1");
+        }while (size <= 0);
 
-        do {
-            System.out.println("Enter N of a MxN matrix: ");
-            N = inputFromUser.nextInt();
-            inputFromUser.nextLine();
-            if (N <= 0) System.out.println("Wrong input: N >= 0");
-        }while (N <= 0);
+        array = new int [size][size];
 
-        initialArray = new int [M][N];
-        transposeArray = new int [N][M];
-
-        for(int i = 0 ; i < M ; i++){
-            for (int j = 0; j < N ; j++){
+        for(int i = 0 ; i < size ; i++){
+            for (int j = 0; j < size ; j++){
                 System.out.println("Please enter the element with coordinates M: " + i + " and N: " + j);
-                transposeArray[j][i] = inputFromUser.nextInt();
+                array[i][j] = inputFromUser.nextInt();
             }
         }
-        System.out.println("Transposed Matrix is:");
 
-        for(int i = 0 ; i < N ; i++){
-            for (int j = 0; j < M ; j++) {
-                System.out.print(transposeArray[i][j] + " ");
+        int sumDiagonal = 0;
+        for(int i = 0 ; i < size ; i++){
+            for (int j = 0; j < size ; j++) {
+                if (i == j) sumDiagonal += array[i][j];
             }
-            System.out.println();
         }
+        System.out.println("Sum of main diagonal: " + sumDiagonal);
 
     }
 }
